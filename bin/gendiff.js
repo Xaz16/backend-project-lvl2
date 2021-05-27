4aa234fbd81b2176bb61316eba24b8d68f464a0a
Xaz16 <gxaz16@gmail.com>
2021-05-27 06:37:12 +0300

    feat: introduce arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,6 @@ const program = new Command();
 
 program.version('0.0.1');
 program.description('Compares two configuration files and shows a difference.');
+program.arguments('<filepath1> <filepath2>');
+program.option('-f, --format [type]', 'output format');
 program.parse(process.argv);
